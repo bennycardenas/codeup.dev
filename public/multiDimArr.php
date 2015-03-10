c6730f398a854ca4d0d39b0685d7e3a2cb0d3118
Benny Cardenas <benitocardenas@gmail.com>
2015-03-10 10:05:44 -0500

    php logical operators

---
+++ public/multiDimArr.php
@@ -0,0 +1,14 @@
+<?php
+
+$test = [
+'person1'=>['last_name' => 'Cards', 'first_name'=>'Benny'], 
+'person2'=>['last_name' => 'Mrtz', 'first_name'=>'Johnny'], 
+'person3'=>['last_name' => 'Rdgz', 'first_name'=>'Marty']
+];
+
+var_dump($test);
+
+print_r($test);
+
+
+?>
